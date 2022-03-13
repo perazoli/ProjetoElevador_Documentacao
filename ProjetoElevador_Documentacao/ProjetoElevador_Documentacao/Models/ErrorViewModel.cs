81015111cbd54a72dba4bcc2d3b22b6b709fc71f
perazoli <perazoli@gmail.com>
2022-03-13 15:51:40 -0300

    documentacao

---
+++ ProjetoElevador_Documentacao/ProjetoElevador_Documentacao/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoElevador_Documentacao.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
